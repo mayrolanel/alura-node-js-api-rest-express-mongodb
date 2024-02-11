68a9eb79a0c2fa103f2c540f99ddf88c371663fb
mayro.belizario <mayro.belizario@trinusco.com.br>
2024-02-11 13:58:03 -0300

    aula-01: criando o projeto com nodejs

--- server.js
+++ server.js
@@ -3,19 +3,6 @@ import app from "./src/app.js"
 
 const PORT = 3000
 
-const rotas = {
-    "/": "Curso de Express API",
-    "/livros": "Livros da Casa do Codigo",
-    "/autores": "Autores da Casa do Codigo"
-};
-
-// const server = http.createServer((req, res) => {
-//     res.writeHead(200, {
-//         "Content-Type":"text/plain"
-//     });
-//     res.end(rotas[req.url]);
-// });
-
 app.listen(PORT, () => {
     console.log("Servidor escutando!");
 })
