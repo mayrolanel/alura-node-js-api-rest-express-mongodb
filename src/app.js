0964ac2318a6b061ad5dce9b93324a868ea1261e
mayro.belizario <mayro.belizario@trinusco.com.br>
2024-02-12 20:34:12 -0300

    aula-03: persistindo dados

--- src/app.js
+++ src/app.js
@@ -1,29 +1,41 @@
 import express from "express";
+import dbConnect from "./config/dbConnect.js";
+import livro from "./models/Livro.js";
+
+const conexao = await dbConnect()
+conexao.on("error", (erro)=> {
+    console.error("Erro de conexão! ", erro)
+})
+
+conexao.once("open", () => {
+    console.log("Conexão com o banco realizada com sucesso!")
+})
 
 const app = express();
 app.use(express.json());
 
-const livros = [
-    {
-        id: 1,
-        titulo: "O senhor dos Aneis"
-    },
-    {
-        id: 2,
-        titulo: "O Hobbit"
-    }
-]
+// const livros = [
+//     {
+//         id: 1,
+//         titulo: "O senhor dos Aneis"
+//     },
+//     {
+//         id: 2,
+//         titulo: "O Hobbit"
+//     }
+// ]
 
-function buscaLivro(id) {
-    return livros.findIndex(livro => livro.id === Number(id))
-}
+// function buscaLivro(id) {
+//     return livros.findIndex(livro => livro.id === Number(id))
+// }
 
 app.get("/", (req, res) => {
     res.status(200).send("Curso de Node.js")
 });
 
-app.get("/livros", (req, res) => {
-    res.status(200).json(livros);
+app.get("/livros", async (req, res) => {
+    const listaLivros = await livro.find({})
+    res.status(200).json(listaLivros)
 });
 
 app.get("/livros/:id", (req, res) => {
